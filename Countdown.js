62fb7916c5999bd0ad32e19bceaa95858d758bd0
Ever Rojas <Everjosejr@hotmail.com>
2022-04-15 19:08:23 -0500

    date updated

--- Countdown.js
+++ Countdown.js
@@ -1,5 +1,5 @@
 const countdown = () => {
-    const countDate = new Date("April 3, 2022 00:00:00").getTime();
+    const countDate = new Date("July 18, 2022 00:00:00").getTime();
     const timeNow = new Date().getTime();
     const gap = countDate - timeNow;
 
